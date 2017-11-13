e503549a6335e5a186920349060db14be2e717a3
richardraviis <33610742+richardraviis@users.noreply.github.com>
2017-11-13 10:24:51 +0530

    Update loginController.js

--- app/scripts/controllers/loginController.js
+++ app/scripts/controllers/loginController.js
@@ -12,12 +12,13 @@ angular.module('userContacts').controller('loginCtrl', ['$scope', '$rootScope', 
 			$rootScope.userName = $scope.userName;
 			var user = $scope.userName;
 			
+			
 			if(data[user]!== undefined && data[user] === $scope.password)
 				{
 				if (typeof(Storage) !== "undefined") {
 				      sessionStorage.setItem("username", $scope.userName);
 				    
-				    document.getElementById("result").innerHTML = sessionStorage.getItem("lastname");
+				    alert(1);
 				}
 				$location.path("/contacts");
 				 
