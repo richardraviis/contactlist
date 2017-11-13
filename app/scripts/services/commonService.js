ac737e0fb825b137ca5782cfb2526a00596a1ec6
richardraviis <33610742+richardraviis@users.noreply.github.com>
2017-11-13 10:09:33 +0530

    Create commonService.js

---
+++ app/scripts/services/commonService.js
@@ -0,0 +1,25 @@
+angular.module('userContacts')
+  .factory('commonService', function($http, $rootScope, $q){
+	  
+	  authenticateUser: function(){
+		var deferred= $q.defer();
+		  //var url= $rootScope.appContext +"/data/Users/"+$rootScope.userName+"contacts.json";
+		  var url= $rootScope.appContext +"/data/login/loginCredentials.json";
+		  
+		  $http.get(url, {
+			  headers:{
+				  'Content-Type': 'application/json',
+				  'Accept': 'application/json',
+					  },
+			  
+			  
+		  }).then(function(data){
+			  
+			  alert(data.data);
+			  deferred.resolve(data.data);
+		  });
+		  
+		  return deferred.promise;
+	  }
+	  
+  })
