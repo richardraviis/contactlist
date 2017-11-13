eb379f76d0922ade3274c19e7a095c2c82e5e1ad
richardraviis <33610742+richardraviis@users.noreply.github.com>
2017-11-13 10:14:04 +0530

    Create appCtrl.js

---
+++ app/scripts/controllers/appCtrl.js
@@ -0,0 +1,14 @@
+'use strict';
+
+angular.module('userContacts').controller('appCtrl', function($scope){
+	
+	$scope.userName="";
+	$scope.password="";
+	$scope.login = function(){
+		alert($scope.userName);
+		alert($scope.userName);
+	};
+		
+	
+	
+});
