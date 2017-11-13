4c524e022a9ce07b6c1194c8256ffe879ff0f6b7
richardraviis <33610742+richardraviis@users.noreply.github.com>
2017-11-13 10:21:16 +0530

    Update contactDetailsController.js

--- app/scripts/controllers/contactDetailsController.js
+++ app/scripts/controllers/contactDetailsController.js
@@ -1,25 +1,12 @@
 'use strict';
 
-angular.module('userContacts').controller('contactDetailsCtrl', ['$scope', '$rootScope', '$location', 'commonService', function($scope, $rootScope, $location, commonService){
+angular.module('userContacts').controller('contactDetailsCtrl', ['$scope', '$rootScope', '$location', 'commonService', '$routeParams', function($scope, $rootScope, $location, commonService, $routeParams){
 	
-		
-		//$location.path("/contacts");
-		commonService.getContacts().then(function(data){
-			
-			$scope.contacts = data;
-			
-			$scope.totalContacts = data.length;
-			
-		});
-		
-		$scope.showDetails = function(id){
-			$scope.id = id;
-			commonService.getContactDetails($scope.id).then(function(data){
-				
+	commonService.getContactDetails($routeParams.id).then(function(data){
 				$scope.contactDetails = data;
-				
+			
 			});
 			
-		};
+		
 			
 }]);
