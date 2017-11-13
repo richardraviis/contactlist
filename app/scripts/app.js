6bd1f46f198e857577869b42bbb777e264551056
richardraviis <33610742+richardraviis@users.noreply.github.com>
2017-11-13 10:05:22 +0530

    Create app.js

---
+++ app/scripts/app.js
@@ -0,0 +1,12 @@
+'use strict'
+
+angular.module('userContacts', ['ngRoute'])
+.config(function($routeProvider, $locationProvider, $qProvider){
+	$routeProvider
+	.when('',{
+		templateUrl:'views/main.html',
+		controller: ''
+		
+	})
+	
+})
