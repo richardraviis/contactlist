0c717cc3f4964a8cad0035ee1b0e8c698a12f325
richardraviis <33610742+richardraviis@users.noreply.github.com>
2017-11-13 10:10:53 +0530

    Update commonService.js

--- app/scripts/services/commonService.js
+++ app/scripts/services/commonService.js
@@ -1,26 +1,59 @@
 angular.module('userContacts')
   .factory('commonService', function($http, $rootScope, $q){
-	  
-	  authenticateUser: function(){
+	 var auth=''; 
+	 return {
+			
+	authenticateUser:function()
+	  {
 		var deferred= $q.defer();
 		  //var url= $rootScope.appContext +"/data/Users/"+$rootScope.userName+"contacts.json";
 		  var url= $rootScope.appContext +"/data/login/loginCredentials.json";
 		  
 		  $http.get(url, {
-			  headers:{
+			  headers: {
 				  'Content-Type': 'application/json',
 				  'Accept': 'application/json',
 					  },
+				data: '',
+				async: false
 			  
 			  
 		  }).then(function(data){
 			  
-			  alert(data.data);
+			  
 			  deferred.resolve(data.data);
 		  });
 		  
 		  return deferred.promise;
-	  }
+	  },
+	 
+	 getContacts: function(){
+		 
+		 var deferred= $q.defer();
+		  var url= $rootScope.appContext +"/data/Users/"+$rootScope.userName+"/contacts.json";
+		 
+		  
+		  $http.get(url, {
+			  headers: {
+				  'Content-Type': 'application/json',
+				  'Accept': 'application/json',
+					  },
+				data: '',
+				async: false
+			  
+			  
+		  }).then(function(data){
+			  
+			  
+			  deferred.resolve(data.data);
+		  });
+		  
+		  return deferred.promise;
+	 }
+	 }
+
+	  
+	 
   
  
 	  
