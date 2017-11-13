bbe1855c7e16f5bc03639856ed63fc33148f2af5
richardraviis <33610742+richardraviis@users.noreply.github.com>
2017-11-13 19:02:16 +0530

    Update contactDetailsController.js

--- app/scripts/controllers/contactDetailsController.js
+++ app/scripts/controllers/contactDetailsController.js
@@ -2,10 +2,22 @@
 
 angular.module('userContacts').controller('contactDetailsCtrl', ['$scope', '$rootScope', '$location', 'commonService', '$routeParams', function($scope, $rootScope, $location, commonService, $routeParams){
 	
-	commonService.getContactDetails($routeParams.id).then(function(data){
-				$scope.contactDetails = data;
+		   
+	        $scope.id = sessionStorage.getItem("id");
+	               
+			commonService.getContactDetails($scope.id).then(function(data){
+				
+				$scope.contact = data;
+				
 			
 			});
+			
+			
+			$scope.goTo = function ( path ) {
+				
+				 				
+				  $location.path( path );
+				};
 			
 		
 			
