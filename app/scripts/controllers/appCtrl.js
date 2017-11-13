b5c0ae793ff2cdf82a91d5e4a8ec77374a763d4e
richardraviis <33610742+richardraviis@users.noreply.github.com>
2017-11-13 10:15:16 +0530

    Update appCtrl.js

--- app/scripts/controllers/appCtrl.js
+++ app/scripts/controllers/appCtrl.js
@@ -1,13 +1,10 @@
 'use strict';
 
-angular.module('userContacts').controller('appCtrl', function($scope){
+angular.module('userContacts').controller('appCtrl', function($rootScope, $scope){
 	
 	$scope.userName="";
 	$scope.password="";
-	$scope.login = function(){
-		alert($scope.userName);
-		alert($scope.userName);
-	};
+	
 		
 	
 	
