cc5a73a7a26b28ee09f5197bbdbb2dc4d7121dda
richardraviis <33610742+richardraviis@users.noreply.github.com>
2017-11-13 10:23:33 +0530

    Update loginController.js

--- app/scripts/controllers/loginController.js
+++ app/scripts/controllers/loginController.js
@@ -1,16 +1,33 @@
 'use strict';
 
-angular.module('userContacts').controller('loginCtrl', ['$scope', '$rootScope', 'commonService','$location', function($scope, $rootScope, commonService, $location){
+angular.module('userContacts').controller('loginCtrl', ['$scope', '$rootScope', '$location', 'commonService', function($scope, $rootScope, $location, commonService){
 	
 	$scope.userName="";
 	$scope.password="";
 	$scope.login = function(){
-		alert($rootScope.appContext);
-		$location.href="/contacts"
+		
+		//$location.path("/contacts");
 		commonService.authenticateUser($scope.userName).then(function(data){
 			
-			console.log("123>>>"+data);
-		})
+			
+			var user = $scope.userName;
+			
+			if(data[user]!== undefined && data[user] === $scope.password)
+				{
+				$location.path("/contacts");
+				 
+				}
+			else {
+				$scope.validationMessage="Invalid Username/Password. Please try again";
+				$scope.userName="";
+				$scope.password="";
+				$location.path("/login");
+				
+			}
+			console.log("123>>>"+data[user]);
+		});
+		
+		
 		console.log($scope.userName);
 		console.log($scope.password);
 	};
